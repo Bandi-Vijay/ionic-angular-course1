d63ca504096d38a939fe5c819a29c10d825b2da4
Bandi-Vijay <vijaykumarbandi321@gmail.com>
2024-09-09 17:40:14 +0530

    recipes project completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RecipeDetailsPageModule } from './recipes/recipe-details/recipe-details.module';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'recipes',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'recipes',
+//     loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+//   },
+// ];
 
 const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule)
+      },
+      {
+        path: ':recipeId',
+        loadChildren:() => import ('./recipes/recipe-details/recipe-details.module').then(m=>RecipeDetailsPageModule)
+         
+      }
+    ]
+  }
 ];
 
 @NgModule({
